267bac6f2ca9582d3c3f34747940a5ed4d9961f2
angelachang <angelachang27@gmail.com>
2017-03-25 12:59:08 -0400

    can now move in direction of camera

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour {
+	private float SPEED = 1;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Successfully loaded whiteboard...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {  
+	  		transform.position = transform.position + transform.forward * SPEED;
+	  	}
+	}
+}
